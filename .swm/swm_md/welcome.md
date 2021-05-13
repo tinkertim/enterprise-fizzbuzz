f8155a1a35730ba78064ffda03e267ddcc8eeda4
Tim Post <tim@swimm.io>
2021-05-13 11:10:20 -0400

    Documentation update.

--- .swm/swm_md/welcome.md
+++
@@ -1,9 +0,0 @@
----
-slug: /
-sidebar_position: 1
----
-
-# Welcome
-
-Use the sidebar to navigate between docs or use the search bar on the topbar.
-
--- src/lib/libfizzbuzz/libfizzbuzz.c
+++ src/lib/libfizzbuzz/libfizzbuzz.c
@@ -11,7 +11,7 @@ void do_fizz_buzz(unsigned int iterations)
   unsigned int i;
   TOP_AND_BOTTOM;
   for(i=1 ; i < iterations - 1; i++)
-    printf("| %-3d | %-4s | %-4s |\n", i, i%3 ? "" : "Fizz", i%5 ? "" : "Buzz");
+    printf("| %-3d | %-4s | %-4s |\n", &i, i%3 ? "" : "Fizz", i%5 ? "" : "Buzz");
   TOP_AND_BOTTOM;
   return 0;
 }
