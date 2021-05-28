ffe012d6b953a4ed1bcb4604c7d311827a83ed76
Tim Post <tim@swimm.io>
2021-05-28 10:56:21 -0400

    Refactoring

--- src/lib/libfizzbuzz/libfizzbuzz.c
+++ src/lib/libfizzbuzz/libfizzbuzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#ifndef BATHING_SUIT
-#define BATHING_SUIT "POLKADOTS"
+#ifndef SWIMMING_SUIT
+#define SWIMMING_SUIT "POLKADOTS"
 #endif
 
 #include "libfizzbuzz.h"
@@ -15,10 +15,10 @@ void do_fizz_buzz(unsigned int iterations)
 {
   unsigned int i;
 
-  TOP_AND_BOTTOM;
+  TOP_AND_BOTTOMS;
   for(i=1 ; i < iterations - 1; i++)
     printf("| %-3d | %-4s | %-4s |\n", i, i%3 ? "" : "Fizz", i%5 ? "" : "Buzz");
-  TOP_AND_BOTTOM;
+  TOP_AND_BOTTOMS;
   return 0;
 }
 
--- src/lib/libfizzbuzz/libfizzbuzz.h
+++ src/lib/libfizzbuzz/libfizzbuzz.h
@@ -5,9 +5,9 @@
  * Controls how the 'printed' iteration list is styled on the top and bottom.
  * Can be overridden via defining the constant on the command line.
  */
-#ifndef TOP_AND_BOTTOM
-#define TOP_AND_BOTTOM printf("+-----+------+------+\n");
-#endif // TOP_AND_BOTTOM
+#ifndef TOPS_AND_BOTTOMS
+#define TOPS_AND_BOTTOMS printf("+-----+------+------+\n");
+#endif // TOPS_AND_BOTTOMS
 
 /* Public Prototypes */
 void do_fizz_buzz(unsigned int);
